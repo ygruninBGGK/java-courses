3e6dce7f1da2a182a8dd539bc482dabb97e085de
Yuri Grunin <ygrunin@xe.com>
2016-11-30 14:09:47 -0500

    asd

--- calculate.java
+++ calculate.java
@@ -2,5 +2,10 @@ public class calculate{
 	
 	public static void main(String[] arg){
 		System.out.println("Calculating....");	
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("The result is ... " +sum);
+
 	} 
 }
